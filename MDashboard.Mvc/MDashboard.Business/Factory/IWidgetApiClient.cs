a9974b804124df71241c69028003843b8c31a0eb
DylanVL05 <127649711+DylanVL05@users.noreply.github.com>
2025-04-16 13:18:02 -0600

    purge

--- MDashboard.Mvc/MDashboard.Business/Factory/IWidgetApiClient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MDashboard.Business.Factory
-{
-    public interface IWidgetApiClient
-    {
-        string Name { get; set; }
-        Task<KeyValuePair<string, object>> ObtenerDatosAsync(string name);
-    }
-}
