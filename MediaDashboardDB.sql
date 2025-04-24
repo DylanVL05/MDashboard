e1413792ee52b86300dd210cd478e613c38a3ae7
Alejrr2 <alejhandrorr@gmail.com>
2025-04-23 19:49:51 -0600

    bd

--- MediaDashboardDB.sql
+++ MediaDashboardDB.sql
@@ -1,12 +1,79 @@
 USE [master]
 GO
-
+/****** Object:  Database [MediaDashboard]    Script Date: 23/4/2025 12:45:15 ******/
 CREATE DATABASE [MediaDashboard]
+GO
+ALTER DATABASE [MediaDashboard] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET AUTO_CLOSE ON 
+GO
+ALTER DATABASE [MediaDashboard] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [MediaDashboard] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [MediaDashboard] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [MediaDashboard] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [MediaDashboard] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [MediaDashboard] SET  MULTI_USER 
+GO
+ALTER DATABASE [MediaDashboard] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [MediaDashboard] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [MediaDashboard] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [MediaDashboard] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [MediaDashboard] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [MediaDashboard] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [MediaDashboard] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [MediaDashboard] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
 GO
 USE [MediaDashboard]
 GO
-
-/****** Object:  Table [dbo].[Components]    Script Date: 16/4/2025 17:50:28 ******/
+/****** Object:  Table [dbo].[Components]    Script Date: 23/4/2025 12:45:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Components](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Tipo] [nvarchar](50) NOT NULL,
@@ -17,8 +84,11 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-/****** Object:  Table [dbo].[ConfiguracionWidgets]    Script Date: 16/4/2025 17:50:28 ******/
+/****** Object:  Table [dbo].[ConfiguracionWidgets]    Script Date: 23/4/2025 12:45:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[ConfiguracionWidgets](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[UsuarioId] [int] NOT NULL,
@@ -33,8 +103,11 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-/****** Object:  Table [dbo].[Logs]    Script Date: 16/4/2025 17:50:28 ******/
+/****** Object:  Table [dbo].[Logs]    Script Date: 23/4/2025 12:45:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Logs](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[UsuarioId] [int] NOT NULL,
@@ -46,8 +119,11 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-/****** Object:  Table [dbo].[PermisosWidgets]    Script Date: 16/4/2025 17:50:28 ******/
+/****** Object:  Table [dbo].[PermisosWidgets]    Script Date: 23/4/2025 12:45:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[PermisosWidgets](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[UsuarioId] [int] NOT NULL,
@@ -61,8 +137,11 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-/****** Object:  Table [dbo].[Usuarios]    Script Date: 16/4/2025 17:50:28 ******/
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 23/4/2025 12:45:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Usuarios](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Nombre] [nvarchar](100) NOT NULL,
@@ -76,8 +155,11 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-/****** Object:  Table [dbo].[Widgets]    Script Date: 16/4/2025 17:50:28 ******/
+/****** Object:  Table [dbo].[Widgets]    Script Date: 23/4/2025 12:45:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Widgets](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Nombre] [nvarchar](100) NOT NULL,
@@ -103,35 +185,13 @@ INSERT [dbo].[Components] ([Id], [Tipo], [Descripcion]) VALUES (3, N'table', N'V
 GO
 SET IDENTITY_INSERT [dbo].[Components] OFF
 GO
-SET IDENTITY_INSERT [dbo].[ConfiguracionWidgets] ON 
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (4, 2, 1, 487, 9321, 0, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (5, 2, 4, 524, 4391, 1, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (6, 2, 3, 1, 1, 0, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (7, 2, 5, 1, 1, 0, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (8, 2, 2, 1, 1, 0, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (9, 2, 8, 1, 1, 1, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (10, 2, 6, 487, 4391, 0, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (11, 3, 7, NULL, NULL, 1, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (12, 3, 2, NULL, NULL, 1, 1)
-GO
-INSERT [dbo].[ConfiguracionWidgets] ([Id], [UsuarioId], [WidgetId], [Height], [Width], [EsFavorito], [EsVisible]) VALUES (13, 2, 9, 524, 4391, 0, 1)
-GO
-SET IDENTITY_INSERT [dbo].[ConfiguracionWidgets] OFF
-GO
 SET IDENTITY_INSERT [dbo].[Usuarios] ON 
 GO
 INSERT [dbo].[Usuarios] ([Id], [Nombre], [Email], [PasswordHash], [Rol], [FechaRegistro]) VALUES (2, N'Alejo', N'Ale@gmail.com', N'123', N'User', NULL)
 GO
 INSERT [dbo].[Usuarios] ([Id], [Nombre], [Email], [PasswordHash], [Rol], [FechaRegistro]) VALUES (3, N'Prueba', N'Prueba@gmail.com', N'123', N'User', CAST(N'2025-04-16T18:02:52.030' AS DateTime))
+GO
+INSERT [dbo].[Usuarios] ([Id], [Nombre], [Email], [PasswordHash], [Rol], [FechaRegistro]) VALUES (5, N'Jose', N'Jose@gmail.com', N'123', N'Admin', CAST(N'2025-04-23T01:34:14.773' AS DateTime))
 GO
 SET IDENTITY_INSERT [dbo].[Usuarios] OFF
 GO
@@ -143,7 +203,7 @@ INSERT [dbo].[Widgets] ([Id], [Nombre], [Descripcion], [ComponentId], [URL_API],
 GO
 INSERT [dbo].[Widgets] ([Id], [Nombre], [Descripcion], [ComponentId], [URL_API], [API_Key], [EsPublico], [Estado]) VALUES (3, N'Rick API', N'Un API de Rick and Morty', 1, N'https://rickandmortyapi.com/api/character/?name=rick&status=alive', N'null', 1, 1)
 GO
-INSERT [dbo].[Widgets] ([Id], [Nombre], [Descripcion], [ComponentId], [URL_API], [API_Key], [EsPublico], [Estado]) VALUES (4, N'JokeApi', N'Api que cuenta chistes.', 1, N'https://v2.jokeapi.dev/joke/Any?lang=es', N'null', 1, 1)
+INSERT [dbo].[Widgets] ([Id], [Nombre], [Descripcion], [ComponentId], [URL_API], [API_Key], [EsPublico], [Estado]) VALUES (4, N'JokeApi', N'Api que cuenta chistes.', 2, N'https://v2.jokeapi.dev/joke/Any?lang=es', N'null', 1, 1)
 GO
 INSERT [dbo].[Widgets] ([Id], [Nombre], [Descripcion], [ComponentId], [URL_API], [API_Key], [EsPublico], [Estado]) VALUES (5, N'MeowFacts API', N'Api que cuenta datos curiosos sobre los gatos', 1, N'https://meowfacts.herokuapp.com/', N'null', 1, 1)
 GO
@@ -163,7 +223,7 @@ SET IDENTITY_INSERT [dbo].[Widgets] OFF
 GO
 SET ANSI_PADDING ON
 GO
-/****** Object:  Index [UQ__Usuarios__A9D10534EDC4BE82]    Script Date: 16/4/2025 17:50:28 ******/
+/****** Object:  Index [UQ__Usuarios__A9D10534EDC4BE82]    Script Date: 23/4/2025 12:45:15 ******/
 ALTER TABLE [dbo].[Usuarios] ADD UNIQUE NONCLUSTERED 
 (
 	[Email] ASC
